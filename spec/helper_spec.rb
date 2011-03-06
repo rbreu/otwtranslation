abc75c1860376102432075e0d275eda7a76e9df5
Rebecca <rebecca@rbreu.de>
2011-03-06 23:44:05 +0100

    rspec test for helper methods t and ts

---
+++ spec/helper_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+include OtwtranslationHelper
+
+describe OtwtranslationHelper do
+
+  describe "ts" do
+    it "should return the original phrase" do
+      ts("Good day!").should == "Good day!"
+    end
+  end
+
+  describe "t" do
+    it "should return the original phrase" do
+      t("home.greeting", :default => "Good day!").should == "Good day!"
+    end
+
+    it "should insert variables" do
+      t("home.greeting", :default => "Good day, %{name}!", :name => "Abby")
+        .should == "Good day, Abby!"
+    end
+  end
+end
