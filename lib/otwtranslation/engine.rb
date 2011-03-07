fd596fd7f9cb206ac95c05a5718c6b28f25d83d0
Rebecca <rebecca@rbreu.de>
2011-03-07 15:28:32 +0100

    typo in rspec test

--- lib/otwtranslation/engine.rb
+++ lib/otwtranslation/engine.rb
@@ -3,9 +3,6 @@ module Otwtranslation
   require "rails"
 
   class Engine < Rails::Engine
-    p config.autoload_paths
-    config.autoload_paths += ["#{config.root}/app/models/otwtranslation"]
-    p config.autoload_paths
   end
 end
 
--- spec/models/translation_key_spec.rb
+++ spec/models/translation_key_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe Otwtranslation::TranslationKeys do
+describe Otwtranslation::TranslationKey do
   pending "add some examples to (or delete) #{__FILE__}"
 end
