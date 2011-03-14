55045645534f2b56bafae78a4997a3cb4be3b680
Rebecca <rebecca@rbreu.de>
2011-03-14 22:34:03 +0100

    made cucumber tests pass for now by loggin in translator as admin

--- features/step_definitions/translator_steps.rb
+++ features/step_definitions/translator_steps.rb
@@ -1,4 +1,10 @@
 Given /^I am a translator$/ do
-  #do nothing...
+  warn "Change translation admin to translator!"
+  user = Factory.create(:user, {:translation_admin => true})
+  visit "/"
+  fill_in "User name", :with => user.login
+  fill_in "Password", :with => user.password
+  click_button "Log in"
+  assert UserSession.find
 end
 
