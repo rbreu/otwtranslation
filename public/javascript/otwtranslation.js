330e425ce54dad20706b20fd4dca55b8e9d098a1
Rebecca <rebecca@rbreu.de>
2011-04-05 21:58:18 +0200

    now you can invoke the inline translator on a phrase you already changed via inline translator

--- public/javascript/otwtranslation.js
+++ public/javascript/otwtranslation.js
@@ -1,26 +1,35 @@
+/*
+Show the inline translator popup
+*/
+function otwtranslation_inline_translator(doc)
+{
+    var phrase_key = $(doc).attr('id').replace("otwtranslation_phrase_", "");
+    var ypos = $(doc).offset().top + $(doc).height() 
+	- $('#main').offset().top + 10
+    var xpos = $(window).width() / 4
+    
+    
+    $.ajax({url: '/translation/phrases/' + phrase_key,
+	    type: 'get',
+	    dataType: 'script',
+	    async: 'false',
+	    data: { '_method': 'show' },
+	    success: function(html) {
+		$('#main').append(html)
+		$('div.otwtranslation_show_inline').css({top: ypos, left: xpos});
+		
+		$('span.otwtranslation_hide_inline').click(function() {
+		    $('div.otwtranslation_show_inline').remove();
+		}); 
+		
+	    }
+	   });   
+}
+
+
 $(document).ready(function() {
   $('span.otwtranslation_mark_untranslated, span.otwtranslation_mark_translated, span.otwtranslation_mark_approved').rightClick(function(event) {
-      var phrase_key = $(this).attr('id').replace("otwtranslation_phrase_", "");
-      var ypos = $(this).offset().top + $(this).height() 
-	  - $('#main').offset().top + 10
-      var xpos = $(window).width() / 4
-
-
-      $.ajax({url: '/translation/phrases/' + phrase_key,
-	      type: 'get',
-	      dataType: 'script',
-	      async: 'false',
-	      data: { '_method': 'show' },
-	      success: function(html) {
-		  $('#main').append(html)
-		  $('div.otwtranslation_show_inline').css({top: ypos, left: xpos});
-		  
-		  $('span.otwtranslation_hide_inline').click(function() {
-		      $('div.otwtranslation_show_inline').remove();
-		  }); 
-
-	      }
-	     });
+      otwtranslation_inline_translator(this)
   });
 })
 
