e48ae63cf9811f64fb4913245debc34024ae7934
Rebecca <rebecca@rbreu.de>
2011-06-06 17:41:11 +0200

    more work on the new rule form.

--- app/controllers/otwtranslation/rules_controller.rb
+++ app/controllers/otwtranslation/rules_controller.rb
@@ -4,11 +4,33 @@ class Otwtranslation::RulesController < ApplicationController
 
 
   def new
-    @rule = Otwtranslation::ContextRule.new
+    @rule = Otwtranslation::ContextRule.new(params[:otwtranslation_context_rule])
   end
 
   def create
-    p params
+    @rule = Otwtranslation::ContextRule.new(params[:otwtranslation_context_rule])
+
+    # OMG refactor! and write tests!
+    
+    conditions = []
+    @rule.conditions.each_slice(2) do |condition, parameters|
+      parameters = Otwtranslation::ParameterParser.tokenize(parameters)
+      conditions << [condition, parameters] unless condition.blank?
+    end
+    @rule.conditions = conditions
+    
+    actions = []
+    @rule.actions.each_slice(2) do |action, parameters|
+      parameters = Otwtranslation::ParameterParser.tokenize(parameters)
+      actions << [action, parameters] unless action.blank?
+    end
+    @rule.actions = actions
+
+    logger.info @rule.conditions
+    logger.info @rule.actions
+    
+
+    redirect_to otwtranslation_language_path(@rule.language_short)
   end
 
   
